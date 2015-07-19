a30582ef65b7f8a62ce3224134e59ee9355069f6
Isidro Lopez <islomar@gmail.com>
2015-07-20 00:18:25 +0200

    Create test for PostMessage action.

---
+++ src/test/java/com/islomar/parrotter/actions/PostMessageShould.java
@@ -0,0 +1,48 @@
+package com.islomar.parrotter.actions;
+
+import com.islomar.parrotter.infrastructure.Console;
+import com.islomar.parrotter.infrastructure.repositories.MessageRepository;
+import com.islomar.parrotter.model.Message;
+
+import org.mockito.Mock;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
+import static org.mockito.Mockito.inOrder;
+import static org.mockito.Mockito.verify;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+@Test
+public class PostMessageShould {
+
+  private static final String ALICE = "Alice";
+  private static final String MESSAGE = "I love the weather today";
+  private static final int FIVE = 5;
+
+  @Mock MessageRepository messageRepository;
+  @Mock Console console;
+
+  private PublishMessage publishMessage;
+
+
+  @BeforeMethod
+  public void setUpMethod() {
+
+    initMocks(this);
+    publishMessage = new PublishMessage(messageRepository, console);
+  }
+
+  public void save_a_message_and_show_it_on_timeline() {
+
+    Message postedMessage = new Message(ALICE, MESSAGE, Instant.now().minus(FIVE, ChronoUnit.SECONDS));
+
+    publishMessage.execute(postedMessage);
+
+    inOrder(messageRepository, console);
+    verify(messageRepository).saveMessage(postedMessage);
+    verify(console).printMessage(MESSAGE + " (" + FIVE + " seconds ago)");
+  }
+}
