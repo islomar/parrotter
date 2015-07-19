61b783d487657aeef05d3a20c86ad714984de88a
Isidro Lopez <islomar@gmail.com>
2015-07-19 21:54:40 +0200

    Change acceptance test

--- src/test/java/com/islomar/parrotter/actions/PublishMessageFeatureTest.java
+++ src/test/java/com/islomar/parrotter/actions/PublishMessageFeatureTest.java
@@ -1,19 +1,20 @@
 package com.islomar.parrotter.actions;
 
-import com.islomar.parrotter.infrastructure.MessageOutput;
+import com.islomar.parrotter.model.MessageOutput;
 import com.islomar.parrotter.model.Message;
-import com.islomar.parrotter.services.CommandLineProcessor;
+import com.islomar.parrotter.commands.CommandLineProcessor;
 
 import org.mockito.Mock;
 import org.testng.annotations.Test;
 
 import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 
 import static org.mockito.Mockito.verify;
 
 
 @Test
-public class PostingFeatureTest {
+public class PublishMessageFeatureTest {
 
   private static final String ALICE = "Alice";
   private static final String MESSAGE = "I love the weather today";
@@ -21,14 +22,14 @@ public class PostingFeatureTest {
   @Mock
   private MessageOutput messageOutput;
 
-  public void a_user_posting_a_message_gets_printed_in_the_console() {
+  public void a_user_publishing_a_message_gets_printed_in_the_console() {
 
     CommandLineProcessor commandLineProcessor = new CommandLineProcessor(messageOutput);
 
     commandLineProcessor.execute(ALICE + " -> " + MESSAGE);
 
-    Message expectedMessage = new Message(ALICE, MESSAGE, Instant.now());
-    verify(messageOutput).printMessage(expectedMessage);
+    Message expectedMessage = new Message(ALICE, MESSAGE, Instant.now().minus(2, ChronoUnit.MINUTES));
+    verify(messageOutput).printMessage(MESSAGE + " (2 minutes ago)");
   }
 
 }
