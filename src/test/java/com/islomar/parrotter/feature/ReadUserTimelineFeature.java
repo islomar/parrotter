1002ebc753a4c0778bad417210779efbc126fad4
Isidro Lopez <islomar@gmail.com>
2015-07-21 21:45:21 +0200

    Test reading 2 messages from a user timeline (instead of one)

--- src/test/java/com/islomar/parrotter/feature/ReadUserTimelineFeature.java
+++ src/test/java/com/islomar/parrotter/feature/ReadUserTimelineFeature.java
@@ -1,16 +1,17 @@
 package com.islomar.parrotter.feature;
 
-import com.islomar.parrotter.infrastructure.formatters.MessageFormatter;
-import com.islomar.parrotter.model.message.MessageService;
-import com.islomar.parrotter.model.user.ShowUserWallService;
 import com.islomar.parrotter.controller.CommandLineProcessor;
 import com.islomar.parrotter.infrastructure.Console;
+import com.islomar.parrotter.infrastructure.formatters.MessageFormatter;
 import com.islomar.parrotter.infrastructure.repositories.MessageRepository;
 import com.islomar.parrotter.infrastructure.repositories.UserRepository;
 import com.islomar.parrotter.model.message.InMemoryMessageRepository;
+import com.islomar.parrotter.model.message.MessageService;
 import com.islomar.parrotter.model.user.InMemoryUserRepository;
+import com.islomar.parrotter.model.user.ShowUserWallService;
 import com.islomar.parrotter.model.user.UserService;
 
+import org.mockito.InOrder;
 import org.mockito.Mock;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -20,16 +21,19 @@ import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.inOrder;
 import static org.mockito.MockitoAnnotations.initMocks;
 
 @Test
 public class ReadUserTimelineFeature {
 
-  private static final String ALICE = "Alice";
-  private static final String MESSAGE_TEXT = "I love the weather today";
+  private static final String BOB = "Bob";
+  private static final String MESSAGE_TEXT_1 = "Damn! We lost!";
+  private static final String MESSAGE_TEXT_2 = "Good game though.";
+  private static final int TWO = 2;
   private static final int FIVE = 5;
   private static final java.time.Instant FIVE_SECONDS_AGO = Instant.now().minus(FIVE, ChronoUnit.SECONDS);
+  private static final java.time.Instant TWO_MINUTES_AGO = Instant.now().minus(TWO, ChronoUnit.MINUTES);
 
   @Mock Console console;
   @Mock Clock clock;
@@ -53,15 +57,18 @@ public class ReadUserTimelineFeature {
     showUserWallService = new ShowUserWallService(messageService, userService, console, messageFormatter);
   }
 
-  public void a_user_publishes_a_message_to_her_personal_timeline() {
+  public void a_user_publishes_two_messages_to_her_personal_timeline() {
 
     CommandLineProcessor commandLineProcessor = new CommandLineProcessor(userService, messageService, showUserWallService);
-    given(clock.instant()).willReturn(FIVE_SECONDS_AGO, Instant.now());
-    commandLineProcessor.execute(ALICE + " -> " + MESSAGE_TEXT);
+    given(clock.instant()).willReturn(TWO_MINUTES_AGO, FIVE_SECONDS_AGO, Instant.now());
+    commandLineProcessor.execute(BOB + " -> " + MESSAGE_TEXT_1);
+    commandLineProcessor.execute(BOB + " -> " + MESSAGE_TEXT_2);
 
-    commandLineProcessor.execute(ALICE);
+    commandLineProcessor.execute(BOB);
 
-    verify(console).printMessage(MESSAGE_TEXT + " (" + FIVE + " seconds ago)");
+    InOrder inOrder = inOrder(console);
+    inOrder.verify(console).printMessage(MESSAGE_TEXT_1 + " (" + 2 + " minutes ago)");
+    inOrder.verify(console).printMessage(MESSAGE_TEXT_2 + " (" + FIVE + " seconds ago)");
   }
 
 }
