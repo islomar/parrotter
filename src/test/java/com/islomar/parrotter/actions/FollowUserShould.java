26893d6f37feef96912cc3efe74fd4390146d33f
Isidro Lopez <islomar@gmail.com>
2015-08-01 14:13:48 +0200

    [feedback-codurance] Fix wrong attribute name in FollowUserShould and assert that a user starts following another user

--- src/test/java/com/islomar/parrotter/actions/FollowUserShould.java
+++ src/test/java/com/islomar/parrotter/actions/FollowUserShould.java
@@ -1,5 +1,7 @@
 package com.islomar.parrotter.actions;
 
+import com.google.common.collect.Sets;
+
 import com.islomar.parrotter.model.user.User;
 import com.islomar.parrotter.model.user.UserService;
 
@@ -10,6 +12,8 @@ import org.testng.annotations.Test;
 import static com.islomar.parrotter.actions.FollowUser.FOLLOWS;
 import static com.islomar.parrotter.actions.PostMessage.POST;
 import static com.islomar.parrotter.actions.ShowUserWall.WALL;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 import static org.mockito.MockitoAnnotations.initMocks;
@@ -19,45 +23,47 @@ import static org.testng.Assert.assertTrue;
 @Test
 public class FollowUserShould {
 
-  private static final String ALICE = "Alice";
-  private static final String BOB = "Bob";
+  private static final String ALICE_USERNAME = "Alice";
+  private static final String BOB_USERNAME = "Bob";
 
   @Mock UserService userService;
-  private FollowUser postMessage;
+  private FollowUser followUser;
 
   @BeforeMethod
   public void setUpMethod() {
     initMocks(this);
 
-    postMessage = new FollowUser(userService);
-
-    given(userService.findUserByUsername(ALICE)).willReturn(new User(ALICE));
+    followUser = new FollowUser(userService);
   }
 
   public void follow_another_user() {
 
-    postMessage.execute(ALICE + FOLLOWS + BOB);
+    User alice = new User(ALICE_USERNAME);
+    given(userService.findUserByUsername(ALICE_USERNAME)).willReturn(alice);
 
-    verify(userService).findUserByUsername(ALICE);
+    followUser.execute(ALICE_USERNAME + FOLLOWS + BOB_USERNAME);
+
+    verify(userService).findUserByUsername(ALICE_USERNAME);
+    assertThat(alice.getFollowedUsers(), is(Sets.newHashSet(BOB_USERNAME)));
   }
 
   public void be_able_to_execute_a_follow_command() {
 
-    assertTrue(postMessage.canExecuteCommandline(ALICE + FOLLOWS + BOB));
+    assertTrue(followUser.canExecuteCommandline(ALICE_USERNAME + FOLLOWS + BOB_USERNAME));
   }
 
   public void not_be_able_to_execute_a_wall_command() {
 
-    assertFalse(postMessage.canExecuteCommandline(ALICE + WALL));
+    assertFalse(followUser.canExecuteCommandline(ALICE_USERNAME + WALL));
   }
 
   public void not_be_able_to_execute_a_post_command() {
 
-    assertFalse(postMessage.canExecuteCommandline(ALICE + POST + "Hello"));
+    assertFalse(followUser.canExecuteCommandline(ALICE_USERNAME + POST + "Hello"));
   }
 
   public void not_be_able_to_execute_a_view_timeline_command() {
 
-    assertFalse(postMessage.canExecuteCommandline(ALICE));
+    assertFalse(followUser.canExecuteCommandline(ALICE_USERNAME));
   }
 }
