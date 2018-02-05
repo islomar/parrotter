1015afa8c0eaa3f9778f5b308186bf01ab0ed2a5
Isidro López <islomar@gmail.com>
2018-02-05 22:58:34 +0100

    [code-quality] Delete unused imports

--- src/test/java/com/islomar/parrotter/actions/utils/CommandRunnerShould.java
+++ src/test/java/com/islomar/parrotter/actions/utils/CommandRunnerShould.java
@@ -5,15 +5,6 @@ import com.islomar.parrotter.actions.FollowUser;
 import com.islomar.parrotter.actions.PostMessage;
 import com.islomar.parrotter.actions.ReadUserPersonalTimeline;
 import com.islomar.parrotter.actions.ShowUserWall;
-import com.islomar.parrotter.infrastructure.Console;
-import com.islomar.parrotter.infrastructure.formatters.MessageFormatter;
-import com.islomar.parrotter.infrastructure.repositories.MessageRepository;
-import com.islomar.parrotter.infrastructure.repositories.UserRepository;
-import com.islomar.parrotter.model.message.InMemoryMessageRepository;
-import com.islomar.parrotter.model.message.MessageService;
-import com.islomar.parrotter.model.user.InMemoryUserRepository;
-import com.islomar.parrotter.model.user.ShowUserWallService;
-import com.islomar.parrotter.model.user.UserService;
 
 import org.mockito.Mock;
 import org.testng.annotations.BeforeMethod;
@@ -27,11 +18,7 @@ import java.util.List;
 import static com.islomar.parrotter.actions.FollowUser.FOLLOWS;
 import static com.islomar.parrotter.actions.PostMessage.POST;
 import static com.islomar.parrotter.actions.ShowUserWall.WALL;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.instanceOf;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Matchers.anyString;
-import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.MockitoAnnotations.initMocks;
 
@@ -43,7 +30,6 @@ public class CommandRunnerShould {
   private static final String ALICE_MESSAGE_TEXT = "I love the weather today";
   private static final java.time.Instant NOW = Instant.now();
 
-  @Mock Console console;
   @Mock Clock clock;
 
   @Mock PostMessage postMessage;
@@ -51,12 +37,7 @@ public class CommandRunnerShould {
   @Mock FollowUser followUser;
   @Mock ShowUserWall showUserWall;
 
-  private MessageService messageService;
-  private ShowUserWallService showUserWallService;
-  private UserService userService;
-
   private CommandRunner commandRunner;
-  private List<Command> commands;
 
 
   @BeforeMethod
@@ -64,7 +45,7 @@ public class CommandRunnerShould {
     initMocks(this);
 
     given(clock.instant()).willReturn(NOW);
-    commands = generateCommands();
+    List<Command> commands = generateCommands();
     commandRunner = new CommandRunner(commands);
   }
 
@@ -97,15 +78,6 @@ public class CommandRunnerShould {
   }
 
   private List<Command> generateCommands() {
-    MessageFormatter messageFormatter = new MessageFormatter(clock);
-
-    MessageRepository messageRepository = new InMemoryMessageRepository();
-    messageService = new MessageService(clock, messageRepository, console, messageFormatter);
-
-    UserRepository userRepository = new InMemoryUserRepository();
-    userService = new UserService(userRepository);
-    showUserWallService = new ShowUserWallService(messageService, userService, console, messageFormatter);
-
     List<Command> commands = new ArrayList<>();
     commands.add(postMessage);
     commands.add(readUserPersonalTimeline);
